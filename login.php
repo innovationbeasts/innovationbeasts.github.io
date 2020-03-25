f890a79a47f50d707862a753e9eb9162629a0f26
innovationbeasts.github.io <62557173+innovationbeasts@users.noreply.github.com>
2020-03-25 18:43:05 +0530

    update

--- login.php
+++ login.php
@@ -1,31 +1,57 @@
-<?php include('server.php') ?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-	 
-  <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
-  </form>
-</body>
-</html>
+<?php
+session_start();
+
+require_once "config.php";
+$username = $password = "";
+$username_err = $password_err = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    if(empty($username_err) && empty($password_err)){
+        $sql = "SELECT id, username, password FROM portal WHERE username = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            $param_username = $username;
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            session_start();
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+                            header("location: index.html");
+
+                        }
+
+                        else{
+
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                }
+
+                 else
+                 {
+
+                    $username_err = "No account found with that username.";
+                 }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+    }
+    mysqli_close($link);
+}
+?>
